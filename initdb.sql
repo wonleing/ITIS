96d392ace113b81f3739d61617d54d3fb818b176
leon <leon@leon-laptop.(none)>
2009-07-29 15:41:44 +0800

    newpj

--- initdb.sql
+++ initdb.sql
@@ -1,8 +1,8 @@
 drop table application;
 create table application (appname varchar(20) primary key, dbname varchar(20), dbuser varchar(20), dbpwd varchar(20), adminuser varchar(20), adminpwd varchar(20), urlbase varchar(40));
 
---drop table project;
---create table project (
+drop table project;
+create table project (pjname varchar(20) primary key, code varchar(20), description varchar(20), people varchar(40), ctime varchar(20), comments varchar(20));
 
 insert into application values ("MediaWiki", "mediawiki", "wikiuser", "secret", "sysop", "itis", NULL);
 insert into application values ("Wordpress", "wordpress", "wordpress", "secret", "admin", "itis", NULL);
@@ -12,3 +12,4 @@ insert into application values ("Dotproject", "dotproject", "dotproject", "secre
 insert into application values ("Orangehrm", "orangehrm", "orangehrm", "secret", "admin", "itis", NULL);
 insert into application values ("Drupal", "drupal", "drupal", "secret", "admin", "itis", NULL);
 
+insert into project values ("itis", "itis", "Initial sample project", "Leon, Nie", "2009-07-29-16:25:17", "Company project");
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #This script is used for create login username and password.
 ((`id -u` != 0)) && echo "You need to be root!" && exit
 yum install -q -y python-cherrypy python-genshi
+cat initdb.sql | sqlite3 db
 python index.py &
 sleep 2
 echo Webserver started!
