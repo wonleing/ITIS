1a71441942656a37571c8857e1b8d8b1051e061b
leon <leon@leon-laptop.(none)>
2009-08-04 20:37:40 +0800

    structure is done

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-#This script is used for create login username and password.
 ((`id -u` != 0)) && echo "You need to be root!" && exit
 rpm -q python-cherrypy
-(($?==1)) && (echo "Intalling python-cherrypy";yum -q -y python-cherrypy)
+(($?==1)) && (echo "Intalling python-cherrypy...";yum -q -y python-cherrypy)
 rpm -q python-genshi
 (($?==1)) && (echo "Intalling python-genshi...";yum -q -y python-genshi)
-rm -rf /etc/itis /usr/itis/script
-cp -r lib/* /usr/lib/python2.*/site-packages
+rm -rf /etc/itis /usr/itis/
+mkdir -p /usr/itis/
+cp -r html index.py language README script test /usr/itis/
+cp lib/* /usr/lib/python2.*/site-packages
 cp -r etc/itis /etc/
-mkdir -p /usr/itis
-cp -r script /usr/itis/
-cat /etc/itis/initdb.sql | sqlite3 db
-echo Package install successfully!
-python index.py &
-sleep 2
-echo Webserver started!
+cat /etc/itis/initdb.sql | sqlite3 /usr/itis/db
+ln -s /usr/itis/script/itis /etc/init.d/itis > /dev/null 2>&1
+echo -e "Package install successfully!\nPlease use '/etc/init.d/itis start' to start the service\n"
